74357932ba4b7079ec2f89e7757144df0d7b83f5
benjamin <benjamin.edgard@gmail.com>
2023-04-03 00:06:20 +0200

    delete

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,27 +1,26 @@
+
 "use strict"
 
+//creation d'un objet vide qui va accueillir le prix : variable globale
+let price = {};
 
-// récupération des données du panier 
 function getCart() {
+  let panier = JSON.parse(localStorage.getItem("panier"));// récupération des données du panier 
+  if (panier === null) {
+    return null
+  }
 
-let panier = JSON.parse(localStorage.getItem("panier"));
-// console.log(panier);
-if (panier === null) {
-  return null
-}
-
-let panierFinal = [];
-for (const sofa of panier) {
-let sofaExistant = panierFinal.find((sofaExistant)=> sofaExistant.id === sofa.id && sofaExistant.couleur === sofa.couleur)
-if (sofaExistant === undefined) 
-{
-  panierFinal.push(sofa)
-}
-else {
-  sofaExistant.quantite += sofa.quantite // si l'id et la couleur sont identique seule la quantité augmente
-}
-}
-return panierFinal
+  let panierFinal = [];
+  for (const sofa of panier) {
+    let sofaExistant = panierFinal.find((sofaExistant) => sofaExistant.id === sofa.id && sofaExistant.couleur === sofa.couleur)
+    if (sofaExistant === undefined) {
+      panierFinal.push(sofa)
+    }
+    else {
+      sofaExistant.quantite += sofa.quantite // si l'id et la couleur sont identique seule la quantité augmente
+    }
+  }
+  return panierFinal
 }
 
 //stockage des données du panier dans une variable
@@ -29,171 +28,158 @@ const panier = getCart();
 if (panier !== null) {
 
   for (const article of panier) {
-    console.log(article)
-     fetch(`http://localhost:3000/api/products/${article.id}`) // ciblage des données de l'API
-    .then (res=> res.json()) //reponse au format Json
-    .then(detailsArticle=> { 
-        // console.log(detailsArticle);
-  
-// création des elements du DOM    
-let section = document.getElementById("cart__items");
+    fetch(`http://localhost:3000/api/products/${article.id}`) // ciblage des données de l'API
+      .then(res => res.json()) //reponse au format Json
+      .then(detailsArticle => {
+        price[article.id] = detailsArticle.price // ajout du prix à l'objet price
 
-let createArticle = document.createElement("article");
-createArticle.classList.add("cart__item") ;
-createArticle.setAttribute("data-id",article.id);
-createArticle.setAttribute("data-color",article.couleur)
+        // création des elements du DOM    
+        let section = document.getElementById("cart__items");
+
+        let createArticle = document.createElement("article");
+        createArticle.classList.add("cart__item");
+        createArticle.setAttribute("data-id", article.id);
+        createArticle.setAttribute("data-color", article.couleur)
+
+        //ajout de la div cart__item__img
+        let cartItemImg = document.createElement("div");
+        cartItemImg.classList.add("cart__item__img");
+        createArticle.appendChild(cartItemImg);
+
+        //creation de l'image dans la div cart__item__img
+        let img = document.createElement("img");
+        img.src = detailsArticle.imageUrl;
+        img.alt = detailsArticle.altTxt;
+        cartItemImg.append(img);
+
+        let divCartItemContent = document.createElement("div");
+        divCartItemContent.classList.add("cart__item__content");
+
+        //ajout de cart__item__content
+        createArticle.append(divCartItemContent);
+
+        //creation de la div cart__item__content__description
+        let cartItemContentDescription = document.createElement("div");
+        cartItemContentDescription.classList.add("cart__item__content__description");
+
+        let h2 = document.createElement("h2");
+        h2.textContent = detailsArticle.name;
+
+        let pColor = document.createElement("p");
+        pColor.textContent = article.couleur;
+
+        let pPrice = document.createElement("p");
+        pPrice.textContent = detailsArticle.price + " €";
+
+        //creation de cart__Item__Content__Settings 
+        cartItemContentDescription.append(h2, pColor, pPrice);
+
+        let cartItemContentSettings = document.createElement("div");
+        cartItemContentSettings.classList.add("cart__item__content__settings");
+
+        //creation de Item Content Settings Quantity
+        let cartItemContentSettingsQuantity = document.createElement("div");
+        let paragraphQuantite = document.createElement("p");
+        paragraphQuantite.textContent = "Qté :";
+
+        //creation de l'input et des attributs 
+        let input = document.createElement("input");
+        input.setAttribute("type", "number");
+        input.setAttribute("class", "itemQuantity");
+        input.setAttribute("name", "itemQuantity");
+        input.setAttribute("min", "1");
+        input.setAttribute("max", "100");
+        input.setAttribute("value", article.quantite);
+        input.addEventListener("change", updatePriceAndQuantity) // creation d'un evenement au changement de prix
 
 
-//ajout de la div cart__item__img
-let cartItemImg = document.createElement("div");
-cartItemImg.classList.add("cart__item__img");
-createArticle.appendChild(cartItemImg);
+        cartItemContentSettingsQuantity.append(paragraphQuantite, input)
+        cartItemContentSettings.append(cartItemContentSettingsQuantity);
 
-//creation de l'image dans la div cart__item__img
-let img = document.createElement("img");
-img.src = detailsArticle.imageUrl;
-img.alt = detailsArticle.altTxt;
-cartItemImg.append(img);
+        //creation de cart__item__content__settings__delete
+        let cartItemContentSettingsDelete = document.createElement("div");
+        cartItemContentSettingsDelete.className = "cart__item__content__settings__delete";
 
+        let pDelete = document.createElement("p");
+        pDelete.classList.add("deleteItem");
+        pDelete.textContent = "Supprimer";
+        cartItemContentSettingsDelete.append(pDelete);
+        cartItemContentSettings.append(cartItemContentSettingsDelete);
 
-let divCartItemContent = document.createElement("div");
-divCartItemContent.classList.add( "cart__item__content");
+        // ajouter un evenement à supprimer
+        pDelete.addEventListener("click", toDelete)
 
-//ajout de cart__item__content
-createArticle.append(divCartItemContent);
-
-//creation de la div cart__item__content__description
-let cartItemContentDescription = document.createElement("div");
-cartItemContentDescription.classList.add("cart__item__content__description");
-
-let h2 = document.createElement("h2");
-h2.textContent = detailsArticle.name;
-
-let pColor = document.createElement("p");
-pColor.textContent = article.couleur;
-
-let pPrice = document.createElement("p");
-pPrice.textContent = detailsArticle.price   + " €";
-
-//creation de cart__Item__Content__Settings 
-cartItemContentDescription.append(h2, pColor, pPrice);
-
-let cartItemContentSettings = document.createElement("div");
-cartItemContentSettings.classList.add("cart__item__content__settings") ;
-
-//creation de Item Content Settings Quantity
-let cartItemContentSettingsQuantity = document.createElement("div");
-let paragraphQuantite = document.createElement("p");
-paragraphQuantite.textContent = "Qté :";
-
-//creation de l'input et des attributs 
-let input = document.createElement("input");
-input.setAttribute("type", "number");
-input.setAttribute("class", "itemQuantity");
-input.setAttribute("name", "itemQuantity");
-input.setAttribute("min", "1");
-input.setAttribute("max", "100");
-input.setAttribute("value", article.quantite); 
-
-// modification de la quantite et du prix
-// input.addEventListener("input", ()=> {
-//   let value = input.value  
-// return value
-// });
-
-input.addEventListener("change", updatePriceAndQuantity) // creation d'un evenement au changement de prix
-
-function updatePriceAndQuantity() { 
-  
-createArticle.closest(".cart-item");  // recupération de l'ID du produit
-console.log(createArticle)
-const itemToSearch = panier.find(item=> item.id === createArticle.dataset.id) // recupération des donnees du panier correspondants à l'ID
-let newQuantity = parseInt(input.value);
-itemToSearch.quantite = newQuantity; 
-quantityTotal(); 
-priceTotal();
-save(panier);
-
-}
-
-// sauvegarde des nouvelles données du panier
-function save(panier) {
-  const dataToSave = JSON.stringify(panier);
-  localStorage.setItem("panier", dataToSave)
-  console.log(dataToSave);
-}
-
-cartItemContentSettingsQuantity.append(paragraphQuantite, input)
-cartItemContentSettings.append(cartItemContentSettingsQuantity);
-
-//creation de cart__item__content__settings__delete
-let cartItemContentSettingsDelete = document.createElement("div");
-cartItemContentSettingsDelete.className = "cart__item__content__settings__delete";
-
-let pDelete = document.createElement("p");
-pDelete.classList.add("deleteItem");
-pDelete.textContent = "Supprimer";
-cartItemContentSettingsDelete.append(pDelete);
-cartItemContentSettings.append(cartItemContentSettingsDelete);
-
-// ajouter un evenement à supprimer
-pDelete.addEventListener("click", toDelete)
-
-function toDelete() {
-  
-  createArticle.closest(".cart-item");   // recupération de l'ID du produit
-  const itemToSearch = panier.find(item=> item.id === createArticle.dataset.id); 
-  createArticle.remove();
-  let newQuantity = input.value = null;
-  itemToSearch.quantite = newQuantity; 
-  quantityTotal();
-  priceTotal();
-
+        divCartItemContent.append(cartItemContentDescription, cartItemContentSettings)
+        // ajout de createArticle a la section cart__items
+        section.append(createArticle);
+        quantityTotal();
+        priceTotal();
+      }
+      )
+      //gestion de l'erreur
+      .catch(() => alert("Désole, il y a une erreur dans l'affichage du produit."))
   }
 
-
-
-divCartItemContent.append(cartItemContentDescription, cartItemContentSettings)
-// ajout de createArticle a la section cart__items
-section.append(createArticle);
 }
-)
-//gestion de l'erreur
-.catch(()=> alert("Désole, il y a une erreur dans l'affichage du produit."))
-}
-quantityTotal();
-priceTotal();
-}
-
-
-//parcourir des produits du panier et ciblage sur l'API
 
 // quantite totale
 function quantityTotal() {
   let total = 0
-  let totalQuantity = document.getElementById("totalQuantity") 
+  let totalQuantity = document.getElementById("totalQuantity")
   panier.forEach(totality => {
-     total+= totality.quantite 
-  }  
+    total += totality.quantite
+  }
   )
   totalQuantity.textContent = total
-
-  };
-
+};
 
 //prix total
 function priceTotal() {
   let totalPrice = document.getElementById("totalPrice");
   let totalSofa = 0
   panier.forEach(sofa => {
-    totalSofa += sofa.quantite * sofa.price
+    totalSofa += sofa.quantite * price[sofa.id]
   });
   totalPrice.textContent = totalSofa;
 }
 
-  
+//augmenter le prix et la quantite
+function updatePriceAndQuantity(e) {
+  let inputQuantite = e.target
+  let article = inputQuantite.closest(".cart__item")
+  let itemToSearch = panier.find(item => item.id === article.dataset.id && article.dataset.color === item.couleur) // recupération des donnees du panier correspondants à l'ID
+  let newQuantity = parseInt(inputQuantite.value);
+  itemToSearch.quantite = newQuantity;
+  quantityTotal();
+  priceTotal();
+  save(panier);
+}
 
+//supprimer le total et le prix
+function toDelete(e) {
+  debugger
+  let btnDelete = e.target; //cibler l'element avec event
+  let article = btnDelete.closest(".cart__item");   // remonter à l'élément parent
+  let itemToSearch = panier.find(item => item.id === article.dataset.id && article.dataset.color === item.couleur); // récupérer l'Id et la couleur
+  console.log(itemToSearch)
+  itemToSearch.quantite = 0;
+  article.remove(); // supprimer le produit
+  quantityTotal(); // actualiser la quantité
+  priceTotal(); // actualiser le prix
 
+  //suppression des donnees du localStorage 
+  let storageData = JSON.parse(localStorage.getItem("panier"));
+  console.log(storageData);
+  let DeleteToLocalStorage = storageData.findIndex(item => item.id === article.dataset.id && article.dataset.color === item.couleur)
+  console.log(DeleteToLocalStorage);
+  storageData.splice(DeleteToLocalStorage, 1); //suppresion de l'element 
+  localStorage.setItem('panier', JSON.stringify(storageData)); // renvoie du panier actualise
+}
 
-
+// sauvegarde des nouvelles données du panier
+function save(panier) {
+  const dataToSave = JSON.stringify(panier); // envoi de la quantite actualisee dans le localStorage
+  localStorage.setItem("panier", dataToSave)
+  console.log(dataToSave);
+}
 
--- front/js/product.js
+++ front/js/product.js
@@ -2,59 +2,55 @@
 //variables globales - global scop 
 let sofa;
 const selectColor = document.getElementById("colors");
-let take_Id; 
+let take_Id;
 
 // Obtenir l'id 
-    const url_id = window.location.href;
-    const url = new URL(url_id);
-    const search_params = new URLSearchParams(url.search);
-    if (search_params.has("id")) {
-         take_Id = search_params.get("id"); // obtention de take_Id dans la portée locale - function scope -
-    }
+const url_id = window.location.href;
+const url = new URL(url_id);
+const search_params = new URLSearchParams(url.search);
+if (search_params.has("id")) {
+  take_Id = search_params.get("id"); // obtention de take_Id dans la portée locale - function scope -
+}
 
 // récupération l'id 
-function getDatas()
-{ 
+function getDatas() {
   return fetch(`http://localhost:3000/api/products/${take_Id}`)
-  .then (res=> res.json()) //reponse au format Json
-  .catch(()=> alert("Désole, il y a une erreur dans l'affichage du produit.")) // error du serveur
+    .then(res => res.json()) //reponse au format Json
+    .catch(() => alert("Désole, il y a une erreur dans l'affichage du produit.")) // error du serveur
 }
 
 
 //récupération des produits 
-function getProducts()
-{ 
-const divImg = document.getElementsByClassName("item__img") [0];
-let img = document.createElement("img");
-img.src =  sofa.imageUrl;
-img.alt = sofa.altTxt;
-divImg.appendChild(img);
+function getProducts() {
+  const divImg = document.getElementsByClassName("item__img")[0];
+  let img = document.createElement("img");
+  img.src = sofa.imageUrl;
+  img.alt = sofa.altTxt;
+  divImg.appendChild(img);
 
 
-let title = document.getElementById("title");
-title.textContent = sofa.name;
+  let title = document.getElementById("title");
+  title.textContent = sofa.name;
 
 
-let price = document.getElementById("price");
-price.textContent = sofa.price;
+  let price = document.getElementById("price");
+  price.textContent = sofa.price;
 
 
-let description = document.getElementById("description");
-description.textContent = sofa.description;
+  let description = document.getElementById("description");
+  description.textContent = sofa.description;
 
-// récupérer les couleurs de l'API
-for ( const color of sofa.colors) {
-  let value = document.createElement("option");
-  value.option = color;
-  selectColor.appendChild(value);
-  selectColor.lastElementChild.innerHTML = color;
-}
+  // récupérer les couleurs de l'API
+  for (const color of sofa.colors) {
+    let value = document.createElement("option");
+    value.option = color;
+    selectColor.appendChild(value);
+    selectColor.lastElementChild.innerHTML = color;
+  }
 }
 
 // afficher les produits dans la page produit
-async function displayProducts ()
-
-{
+async function displayProducts() {
   sofa = await getDatas(); // attente des données de l'API
   getProducts();  // les éléments selectionnés à partir du DOM
 }
@@ -63,65 +59,63 @@ displayProducts(); // affichage du produit
 
 // selection des donnees  
 let addToCart = document.getElementById("addToCart");
-let color = document.getElementById("colors"); 
-let quantity = document.getElementById("quantity"); 
+let color = document.getElementById("colors");
+let quantity = document.getElementById("quantity");
 
 
 function array() {
-  
+
   let clePanier = "panier";
-let cart = JSON.parse(localStorage.getItem(clePanier))
-console.log(cart); //obtenir les éléments du panier
+  let cart = JSON.parse(localStorage.getItem(clePanier))
+  console.log(cart); //obtenir les éléments du panier
 
-// creation du panier 
-const sofaToAdd = {
-       id : take_Id,
-    couleur : color.value,
-    quantite : parseInt(quantity.value), // transformer un string en number
-    price : sofa.price  
-}
+  // creation du panier 
+  const sofaToAdd = {
+    id: take_Id,
+    couleur: color.value,
+    quantite: parseInt(quantity.value), // transformer un string en number
+  }
 
-if (cart === null) {  // si local storage ne renvoie rien, ajout du panier
-  cart = [sofaToAdd];
-}
-else { 
-let sofa =  cart.find((sofa)=> sofa.couleur === sofaToAdd.couleur && sofa.id=== sofaToAdd.id)
- if (sofa === undefined) { // si aucune correspondance n'est trouvé ajout au panier du prouit
-  cart.push(sofaToAdd);
- }
- else {
-sofa.quantite+= sofaToAdd.quantite; // sinon simple modification de la quantité
- }
-}
-  
- localStorage.setItem(clePanier, JSON.stringify(cart)) // le local storage n'admet qu'un objet JSON
- return cart 
+  if (cart === null) {  // si local storage ne renvoie rien, ajout du panier
+    cart = [sofaToAdd];
+  }
+  else {
+    let sofa = cart.find((sofa) => sofa.couleur === sofaToAdd.couleur && sofa.id === sofaToAdd.id)
+    if (sofa === undefined) { // si aucune correspondance n'est trouvé ajout au panier du prouit
+      cart.push(sofaToAdd);
+    }
+    else {
+      sofa.quantite += sofaToAdd.quantite; // sinon simple modification de la quantité
+    }
+  }
+
+  localStorage.setItem(clePanier, JSON.stringify(cart)) // le local storage n'admet qu'un objet JSON
+  return cart
 }
 
 // selection des donnees
 function selectDatas() {
 
-  if (color.value == null || color.value == '')  { 
+  if (color.value == null || color.value == '') {
 
     alert("Choose a color");
   }
-  
-  else if (quantity.value == null || quantity.value == 0 || quantity.value > 100) 
-  {
+
+  else if (quantity.value == null || quantity.value == 0 || quantity.value > 100) {
     alert("choose a quantity between 1 and 100");
   }
   else {
 
-    let storage= array();
+    let storage = array();
     console.log(storage);
     alert("Your(s) product(s) has been added to the card")
- 
+
   }
 
 }
 
 // l'evenement click 
-addToCart.addEventListener("click",selectDatas) 
+addToCart.addEventListener("click", selectDatas)
 
 
 
